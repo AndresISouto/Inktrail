cd1d43aaa982a07c66b88293df21bdfad4e07a86
AndresISouto <AndresISouto@users.noreply.github.com>
2026-01-25 16:51:13 +0100

    book type

---
+++ frontend/src/types/book.type.ts
@@ -0,0 +1,11 @@
+export interface Book {
+  id: string;
+  ISBN: string;
+  BookTitle: string;
+  BookAuthor: string;
+  YearOfPublication: string;
+  Publisher: string;
+  ImageURLS: string;
+  ImageURLM: string;
+  ImageURLL: string;
+}
