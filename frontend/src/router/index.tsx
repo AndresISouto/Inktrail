502478d854fb086ade05412ba7a30197a587180f
AndresISouto <AndresISouto@users.noreply.github.com>
2026-01-25 16:51:08 +0100

    router

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,7 +1,31 @@
 import { createBrowserRouter } from "react-router";
+import { RootLayout } from "../layouts/RootLayout";
+import { HomePage } from "../components/home/HomePage";
+import { BookDetail } from "../components/detail/BookDetail";
+import { BooksPage } from "../components/pages/BooksPage";
+import { AboutPage } from "../components/pages/AboutPage";
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <h1>hola</h1>,
-  }]);
+    element: <RootLayout></RootLayout>,
+    children: [
+      {
+        index: true,
+        element: <HomePage></HomePage>
+      },
+      {
+        path: 'book/:title',
+        element: <BookDetail></BookDetail>
+      },
+      {
+        path: 'libros',
+        element: <BooksPage></BooksPage>
+      },
+      {
+        path: 'about',
+        element: <AboutPage></AboutPage>
+      }
+    ]
+  }
+]);
