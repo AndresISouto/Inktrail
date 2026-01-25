ca9b6df1dce9df36943f45fe48200653008d41b3
AndresISouto <AndresISouto@users.noreply.github.com>
2026-01-25 16:50:52 +0100

    links

---
+++ frontend/src/const/links.ts
@@ -0,0 +1,17 @@
+export const links = [
+  {
+    id: 1,
+    title: "Inicio",
+    href: '/'
+  },
+  {
+    id: 2,
+    title: "Libros",
+    href: '/libros'
+  },
+  {
+    id: 3,
+    title: "Sobre Nosotros",
+    href: '/about'
+  },
+]
