a943f4f55e2c0290d83139675bdb27b8dc423ddc
AndresISouto <AndresISouto@users.noreply.github.com>
2026-02-02 08:45:01 +0100

    add book page

--- frontend/src/components/pages/BooksPage.tsx
+++ frontend/src/components/pages/BooksPage.tsx
@@ -1,13 +1,13 @@
+import { fetchBooksPaginated } from "@/api/bookQueries";
+import { useQuery } from "@tanstack/react-query";
+
 export const BooksPage = () => {
-  return (
-    <div className="max-w-6xl mx-auto p-6">
-      <h1 className="text-3xl font-bold text-gray-800 mb-6">All Books</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        {/* Book cards will be mapped here */}
-        <p className="col-span-full text-gray-600 text-center py-12">
-          Books coming soon...
-        </p>
-      </div>
-    </div>
-  );
+  const { data, error, isPending, isError } = useQuery({
+    queryKey: ["allBooksPaged"],
+    queryFn: () => fetchBooksPaginated(1),
+  });
+  console.log(data);
+
+  return <h1>flsajfl</h1>;
 };
+
