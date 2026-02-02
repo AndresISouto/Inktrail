043d0cf0e0b4a8ba78f5b3b09ac4152fe762cb90
AndresISouto <AndresISouto@users.noreply.github.com>
2026-02-02 08:46:00 +0100

    main

---
+++ backend/src/main/java/com/inktrail/inktrail/InktrailApplication.java
@@ -0,0 +1,13 @@
+package com.inktrail.inktrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InktrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InktrailApplication.class, args);
+	}
+
+}
