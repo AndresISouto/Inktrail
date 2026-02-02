44c7df41ff19d42a75ab8635111722e4f266819c
AndresISouto <AndresISouto@users.noreply.github.com>
2026-02-02 08:46:17 +0100

    back

---
+++ backend/src/test/java/com/inktrail/inktrail/InktrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inktrail.inktrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InktrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
