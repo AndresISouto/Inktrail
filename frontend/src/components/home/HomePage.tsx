9d32cd8f4eaeab1c57c7521bcf6be3b406f444cf
AndresISouto <AndresISouto@users.noreply.github.com>
2026-02-02 08:45:08 +0100

    add homepage

--- frontend/src/components/home/HomePage.tsx
+++ frontend/src/components/home/HomePage.tsx
@@ -1,32 +1,29 @@
-import { Banner } from "./Banner"
-import { books } from '../../data/bookData'
-import { BookCard } from "../shared/BookCard"
+import { Banner } from "./Banner";
+import { books } from "../../data/bookData";
+import { BookCard } from "../shared/BookCard";
 
 export const HomePage = () => {
-
-
   return (
     <>
       <Banner></Banner>
       <section>
-        <h2 className="text-center font-semibold text-4xl p-4 m-4">Los mas vendidos</h2>
-        <article className="flex items-center justify-around">
-          {
-            books.map(book => (
-              <BookCard book={book}></BookCard>
-            ))
-          }
+        <h2 className="text-center font-semibold text-4xl p-4 m-4">
+          Los mas vendidos
+        </h2>
+        <article className="flex items-center justify-around p-4">
+          {books.map((book) => (
+            <BookCard book={book}></BookCard>
+          ))}
         </article>
-        <h2 className="text-center font-semibold text-4xl p-4 m-4">Ultimas novedades</h2>
-        <article className="flex items-center justify-around">
-          {
-            books.map(book => (
-              <BookCard book={book}></BookCard>
-            ))
-          }
+        <h2 className="text-center font-semibold text-4xl p-4 m-4">
+          Ultimas novedades
+        </h2>
+        <article className="flex items-center justify-around p-4">
+          {books.map((book) => (
+            <BookCard book={book}></BookCard>
+          ))}
         </article>
       </section>
-
     </>
-  )
-}
+  );
+};
