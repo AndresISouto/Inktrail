22fb56570dc99550f7dec215a28e599d63e71919
AndresISouto <AndresISouto@users.noreply.github.com>
2026-02-03 12:26:24 +0100

    types

--- frontend/src/api/bookQueries.ts
+++ frontend/src/api/bookQueries.ts
@@ -1,4 +1,8 @@
-export const fetchBooksPaginated = async (page: number) => {
+import type { PaginatedBooksResponse } from "@/types/book.type";
+
+export const fetchBooksPaginated = async (
+  page: number,
+): Promise<PaginatedBooksResponse> => {
   const response = await fetch(`http://localhost:8080/api/books?page=${page}`);
 
   return response.json();
