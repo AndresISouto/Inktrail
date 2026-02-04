e41bdc64f4ce456b434234629bfd328920e91958
AndresISouto <AndresISouto@users.noreply.github.com>
2026-02-04 10:33:30 +0100

    removed unused imports

--- backend/src/main/java/com/inktrail/inktrail/repository/BookRepository.java
+++ backend/src/main/java/com/inktrail/inktrail/repository/BookRepository.java
@@ -2,8 +2,6 @@ package com.inktrail.inktrail.repository;
 
 import com.inktrail.inktrail.model.Book;
 
-import org.springframework.boot.data.autoconfigure.web.DataWebProperties.Pageable;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
