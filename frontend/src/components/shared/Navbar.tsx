a7301d1addcf0d8904fe5cc3a464454b95dbfaf3
AndresISouto <AndresISouto@users.noreply.github.com>
2026-02-17 18:16:55 +0100

    log in state

--- frontend/src/components/shared/Navbar.tsx
+++ frontend/src/components/shared/Navbar.tsx
@@ -1,23 +1,37 @@
-import { useState } from 'react'
-import { Link, NavLink } from 'react-router'
-import { FiSearch, FiShoppingCart, FiMenu, FiX } from 'react-icons/fi'
-import { links } from '../../const/links'
+import { useState } from "react";
+import { Link, NavLink, useNavigate } from "react-router";
+import { FiSearch, FiShoppingCart, FiMenu, FiX } from "react-icons/fi";
+import { links } from "../../const/links";
+import { useUserStore } from "@/globalState/user";
 
 export default function Navbar() {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
+  const userId = useUserStore((state) => state.userId);
 
   const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen)
-  }
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
+  const handleSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && searchQuery.trim()) {
+      navigate(`/libros?title=${searchQuery.trim()}`);
+      setSearchQuery("");
+    }
+  };
 
   return (
     <nav className="bg-white shadow-md ">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
-          <div className="flex-shrink-0">
-            <Link to="/" className="text-3xl font-bold text-gray-800" style={{ fontFamily: 'Dancing Script' }}>
+          <div className="shrink-0">
+            <Link
+              to="/"
+              className="text-3xl font-bold text-gray-800"
+              style={{ fontFamily: "Dancing Script" }}
+            >
               InkTrail
             </Link>
           </div>
@@ -25,33 +39,51 @@ export default function Navbar() {
           {/* Desktop Navigation */}
           <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-4">
-              {
-                links.map(link => (
-                  <NavLink
-                    key={link.id}
-                    to={link.href}
-                    className={({ isActive }) => (
-                      isActive ? "text-cyan-600 underline" :
-                        "text-gray-700 px-3 py-2 rounded-md text-sm font-medium" +
+              {links.map((link) => (
+                <NavLink
+                  key={link.id}
+                  to={link.href}
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-cyan-600 underline"
+                      : "text-gray-700 px-3 py-2 rounded-md text-sm font-medium" +
                         "hover:underline hover:text-cyan-500 transition-all duration-300"
-                    )}
-                  >{link.title}</NavLink>
-                ))
-              }
+                  }
+                >
+                  {link.title}
+                </NavLink>
+              ))}
             </div>
           </div>
 
           {/* Desktop Icons */}
           <div className="hidden md:flex items-center space-x-4">
-            <button className="text-gray-700 hover:text-gray-900 p-2">
-              <FiSearch size={20} />
-            </button>
-            <button className="text-gray-700 hover:text-gray-900 p-2 relative">
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="Buscar libros..."
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                onKeyDown={handleSearch}
+                className="pl-8 pr-4 py-1.5 border border-gray-300 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-transparent"
+              />
+              <FiSearch
+                className="absolute left-2.5 top-1/2 transform -translate-y-1/2 text-gray-400"
+                size={16}
+              />
+            </div>
+            {userId === null && (
+              <Link key={"login"} to={"/login"}>
+                Log in
+              </Link>
+            )}
+            <Link
+              key={"carrito"}
+              to={"/cart"}
+              className="text-gray-700 hover:text-gray-900 p-2 relative"
+            >
               <FiShoppingCart size={20} />
-              <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
-                0
-              </span>
-            </button>
+            </Link>
           </div>
 
           {/* Mobile menu button */}
@@ -69,32 +101,42 @@ export default function Navbar() {
         {isMobileMenuOpen && (
           <div className="md:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-              {
-                links.map(link => (
-                  <Link
-                    to={link.href}
-                    className="text-gray-700 hover:text-gray-900 block px-3 py-2 rounded-md text-base font-medium"
-                    onClick={() => setIsMobileMenuOpen(false)}
-                  >
-                    {link.title}
-                  </Link>
-                ))
-              }
+              {links.map((link) => (
+                <Link
+                  to={link.href}
+                  className="text-gray-700 hover:text-gray-900 block px-3 py-2 rounded-md text-base font-medium"
+                  onClick={() => setIsMobileMenuOpen(false)}
+                >
+                  {link.title}
+                </Link>
+              ))}
               <div className="flex items-center space-x-4 px-3 py-2">
-                <button className="text-gray-700 hover:text-gray-900 p-2">
-                  <FiSearch size={20} />
-                </button>
-                <button className="text-gray-700 hover:text-gray-900 p-2 relative">
+                <div className="relative flex-1">
+                  <input
+                    type="text"
+                    placeholder="Buscar libros..."
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    onKeyDown={handleSearch}
+                    className="w-full pl-8 pr-4 py-1.5 border border-gray-300 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-transparent"
+                  />
+                  <FiSearch
+                    className="absolute left-2.5 top-1/2 transform -translate-y-1/2 text-gray-400"
+                    size={16}
+                  />
+                </div>
+                <Link
+                  key={"cart"}
+                  to={"/cart"}
+                  className="text-gray-700 hover:text-gray-900 p-2 relative"
+                >
                   <FiShoppingCart size={20} />
-                  <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
-                    0
-                  </span>
-                </button>
+                </Link>
               </div>
             </div>
           </div>
         )}
       </div>
     </nav>
-  )
+  );
 }
