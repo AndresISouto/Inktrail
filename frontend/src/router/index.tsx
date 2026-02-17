e69de92606a0939e2812bf408dd510f994d915f4
AndresISouto <AndresISouto@users.noreply.github.com>
2026-02-17 18:17:03 +0100

    some routes

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -4,6 +4,9 @@ import { HomePage } from "../components/home/HomePage";
 import { BookDetail } from "../components/detail/BookDetail";
 import { BooksPage } from "../components/pages/BooksPage";
 import { AboutPage } from "../components/pages/AboutPage";
+import { LoginPage } from "@/components/pages/LoginPage";
+import { SignInPage } from "@/components/pages/SignInPage";
+import { CartPage } from "@/components/pages/CartPage";
 
 export const router = createBrowserRouter([
   {
@@ -26,6 +29,18 @@ export const router = createBrowserRouter([
         path: "about",
         element: <AboutPage></AboutPage>,
       },
+      {
+        path: "cart",
+        element: <CartPage></CartPage>,
+      },
     ],
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/register",
+    element: <SignInPage></SignInPage>,
   },
 ]);
